e09cb95d1f267591450ac1e39800679bba12f200
Abram Fernando Haullussy <abramfernando18@gmail.com>
2025-06-03 18:58:43 +0700

    feat(app/cms/admins): add cms list admin page component

---
+++ front-end/app/cms/admins/page.tsx
@@ -0,0 +1,33 @@
+'use client'
+import CMSLayout from '@/layouts/cms'
+import Table, {TableHeader} from '@/components/cms/Table'
+import { BreadcrumbData } from "@/components/cms/Breadcrumbs"
+
+export default function Admin() {
+  const breadcrumbsPages: BreadcrumbData[] = [
+    { name: 'List Admin', path: '/cms/admins', current: true }
+  ]
+  const tableHeaders: TableHeader[] = [
+    { name: 'Nama' },
+    { name: 'Email' },
+    { name: 'Action' },
+  ]
+
+  return (
+    <CMSLayout pages={breadcrumbsPages}>
+      <Table
+        title='List Admin'
+        description='list manajemen admin'
+        addButton={{
+          path: '/cms/admins/create',
+        }}
+        headers={tableHeaders}
+        data={[
+          { name: 'Admin1', email: 'admin1@mail.com' },
+          { name: 'Admin1', email: 'admin1@mail.com' },
+          { name: 'Admin1', email: 'admin1@mail.com' },
+        ]}
+      />
+    </CMSLayout>
+  )
+}
