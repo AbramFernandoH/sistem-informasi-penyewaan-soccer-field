f624dd89cf609d88c20cc354b30f5f2e71d25426
Abram Fernando Haullussy <abramfernando18@gmail.com>
2025-06-09 19:46:16 +0700

    refactor(app/cart): adjust cart page component

--- front-end/app/cart/page.tsx
+++ front-end/app/cart/page.tsx
@@ -5,8 +5,8 @@ import React from 'react'
 export default function Cart() {
   return (
     <PwaLayout>
-      <div className='w-full max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 mx-auto flex flex-col space-y-6'>
-        <h2 className='text-2xl font-bold text-gray-900 text-center mt-8'>Keranjang</h2>
+      <div className='w-full max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 mx-auto flex flex-col space-y-6 mt-8 lg:mt-0'>
+        <h2 className='text-2xl font-bold text-gray-900 text-center lg:text-3xl'>Keranjang</h2>
 
         <CartList />
 
