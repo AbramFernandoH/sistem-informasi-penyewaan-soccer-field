6b372c52f80bc69b6fe07c5a8f2dd9c33e187b4e
Abram Fernando Haullussy <abramfernando18@gmail.com>
2025-06-16 18:21:07 +0700

    feat(front-end/app/cms, front-end/components): add delete admin api implementation

--- front-end/app/cms/admins/page.tsx
+++ front-end/app/cms/admins/page.tsx
@@ -3,33 +3,81 @@ import CMSLayout from '@/layouts/cms'
 import Table from '@/components/cms/Table'
 import { BreadcrumbData } from '@/components/cms/Breadcrumbs'
 import Link from 'next/link'
-import { PencilIcon } from '@heroicons/react/24/outline'
+import { PencilIcon, TrashIcon } from '@heroicons/react/24/outline'
 import { ReactNode, useEffect, useState } from 'react'
-import { useQuery } from '@tanstack/react-query'
-import { ListAdminRequest, ListAdminResponse } from '@/utils/type'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import type { Admin, ListAdminRequest, ListAdminResponse } from '@/utils/type'
 import { fetchWithAuth } from '@/utils/helper'
 import { ENV } from '@/utils/constants'
+import DeleteModal from '@/components/DeleteModal'
+import toast from 'react-hot-toast'
+import { adminProfileStore } from '@/stores/adminProfile'
 
 export default function Admin() {
+  const queryClient = useQueryClient()
+
+  const adminProfile = adminProfileStore((state) => state.admin)
+
   const [skip, setSkip] = useState(0)
   const [page, setPage] = useState(1)
   const [tableData, setTableData] = useState<(string | ReactNode)[][]>([])
+  const [isOpenDeleteModal, setIsOpenDeleteModal] = useState(false)
+  const [selectedAdmin, setSelectedAdmin] = useState<Admin | null>(null)
 
   const breadcrumbsPages: BreadcrumbData[] = [{ name: 'List Admin', path: '/cms/admins', current: true }]
   const tableHeaders = ['Nama Lengkap', 'Username', 'Action']
 
-  const { data, isSuccess } = useQuery<ListAdminRequest, unknown, ListAdminResponse>({
-    queryKey: ['admin', skip],
-    queryFn: async () => {
-      const res = await fetchWithAuth('cms', `${ENV.API_URL}/admins?skip=${skip}`)
+  const { data: dataListAdmin, isSuccess: isSuccessListAdmin } = useQuery<ListAdminRequest, unknown, ListAdminResponse>(
+    {
+      queryKey: ['admin', skip],
+      refetchOnWindowFocus: false,
+      queryFn: async () => {
+        const res = await fetchWithAuth('cms', `${ENV.API_URL}/admins?skip=${skip}`)
 
-      if (!res.ok) {
-        const errorData = await res.json()
+        if (!res.ok) {
+          const errorData = await res.json()
 
-        throw new Error(errorData.message || 'Failed to get list admin')
+          throw new Error(errorData.message || 'Failed to get list admin')
+        }
+
+        return res.json()
+      },
+    }
+  )
+
+  const {
+    isPending: isPendingDeleteAdmin,
+    isSuccess: isSuccessDeleteAdmin,
+    reset: resetDeleteAdmin,
+    mutate: mutateDeleteAdmin,
+  } = useMutation({
+    mutationFn: async () => {
+      const response = await fetchWithAuth('cms', `${ENV.API_URL}/admins/${selectedAdmin?.username ?? ''}`, {
+        method: 'DELETE',
+      })
+
+      const json = await response.json()
+
+      if (!response.ok) {
+        // Attach the JSON error message if needed
+        throw new Error(json.message || 'Delete admin failed')
       }
 
-      return res.json()
+      return json
+    },
+    onSuccess: async () => {
+      // Invalidate and refetch
+      await queryClient.invalidateQueries({ queryKey: ['admin'] })
+
+      toast.success('Hapus admin berhasil')
+
+      setIsOpenDeleteModal(false)
+      setSelectedAdmin(null)
+
+      resetDeleteAdmin()
+    },
+    onError: () => {
+      toast.error('Gagal hapus admin')
     },
   })
 
@@ -43,41 +91,81 @@ export default function Admin() {
     setPage(page + 1)
   }
 
+  const handleClickOpenDeleteModal = (admin: Admin) => () => {
+    setIsOpenDeleteModal(true)
+
+    setSelectedAdmin(admin)
+  }
+
+  const handleCloseDeleteModal = () => {
+    setIsOpenDeleteModal(false)
+  }
+
+  const handleClickDelete = () => {
+    mutateDeleteAdmin()
+  }
+
   useEffect(() => {
-    if (isSuccess) {
+    if (isSuccessListAdmin) {
       setTableData(
-        data.data.items.map((item) => [
+        dataListAdmin.data.items.map((item) => [
           item.fullName,
           item.username,
-          <Link
+          <div
             key={item._id}
-            href={`/cms/admins/${item.username}/edit`}
-            className='w-fit flex items-center space-x-2 rounded-md bg-indigo-600 px-3 py-2 text-center text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+            className='flex items-center space-x-2'
           >
-            <PencilIcon className='size-3' />
+            <Link
+              href={`/cms/admins/${item.username}/edit`}
+              className='w-fit flex items-center space-x-2 rounded-md bg-orange-500 px-3 py-2 text-center text-xs font-semibold text-white shadow-sm hover:bg-orange-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+            >
+              <PencilIcon className='size-3' />
 
-            <span>Edit</span>
-          </Link>,
+              <span>Edit</span>
+            </Link>
+
+            {((adminProfile && adminProfile._id !== item._id) || adminProfile === null) &&
+              dataListAdmin.data.items.length > 1 && (
+                <button
+                  className='w-fit flex items-center space-x-2 rounded-md bg-red-600 px-3 py-2 text-center text-xs font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:bg-gray-300 disabled:cursor-not-allowed'
+                  onClick={handleClickOpenDeleteModal(item)}
+                >
+                  <TrashIcon className='size-3' />
+
+                  <span>Hapus</span>
+                </button>
+              )}
+          </div>,
         ])
       )
     }
-  }, [data?.data.items, isSuccess])
+  }, [adminProfile, dataListAdmin?.data.items, isSuccessListAdmin])
 
   return (
-    <CMSLayout pages={breadcrumbsPages}>
-      <Table
-        title='List Admin'
-        description='list manajemen admin'
-        addButton={{
-          path: '/cms/admins/create',
-        }}
-        headers={tableHeaders}
-        data={tableData}
-        totalData={data?.data.metadata.count ?? 0}
-        currentPage={page}
-        handleClickPrev={handleClickPrev}
-        handleClickNext={handleClickNext}
+    <>
+      <CMSLayout pages={breadcrumbsPages}>
+        <Table
+          title='List Admin'
+          description='list manajemen admin'
+          addButton={{
+            path: '/cms/admins/create',
+          }}
+          headers={tableHeaders}
+          data={tableData}
+          totalData={dataListAdmin?.data.metadata.count ?? 0}
+          currentPage={page}
+          handleClickPrev={handleClickPrev}
+          handleClickNext={handleClickNext}
+        />
+      </CMSLayout>
+
+      <DeleteModal
+        text={selectedAdmin?.fullName ?? ''}
+        isOpen={isOpenDeleteModal}
+        closeModal={handleCloseDeleteModal}
+        disabled={isPendingDeleteAdmin || isSuccessDeleteAdmin}
+        action={handleClickDelete}
       />
-    </CMSLayout>
+    </>
   )
 }
---
+++ front-end/components/DeleteModal.tsx
@@ -0,0 +1,62 @@
+'use client'
+import { FC } from 'react'
+import Modal from '@/components/Modal'
+import { TrashIcon } from '@heroicons/react/24/outline'
+
+type FieldOrderModalProps = {
+  isOpen: boolean
+  text: string
+  closeModal: () => void
+  action: () => void
+  disabled?: boolean
+}
+
+const FieldOrderModal: FC<FieldOrderModalProps> = ({ isOpen, text, closeModal, action, disabled = false }) => {
+  const onClose = () => {
+    if (!disabled) {
+      closeModal()
+    }
+  }
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      className='!w-[calc(100vw-32px)] sm:max-w-[600px] lg:max-w-[584px] p-5 lg:p-10'
+    >
+      <div className='text-center'>
+        <div className='relative flex items-center justify-center z-[1] mb-7'>
+          <div className='rounded-full w-[90px] h-[90px] p-3 flex justify-center items-center bg-red-600'>
+            <TrashIcon className='size-12 text-white font-semibold' />
+          </div>
+        </div>
+
+        <h4 className='mb-2 text-2xl font-semibold text-gray-800 sm:text-title-sm'>Konfirmasi Hapus</h4>
+
+        <p className='text-sm leading-6 text-gray-500'>Apakah anda yakin menghapus {text}?</p>
+
+        <div className='flex items-center justify-center w-full gap-3 mt-7'>
+          <button
+            type='button'
+            className='flex justify-center w-full px-4 py-3 text-sm font-medium rounded-lg bg-white text-red-500 border border-solid border-red-500 shadow-theme-xs hover:bg-error-600 sm:w-auto disabled:border-0 disabled:text-white disabled:bg-gray-300 disabled:cursor-not-allowed'
+            onClick={closeModal}
+            disabled={disabled}
+          >
+            Cancel
+          </button>
+
+          <button
+            type='button'
+            className='flex justify-center w-full px-4 py-3 text-sm font-medium text-white rounded-lg bg-red-500 shadow-theme-xs hover:bg-error-600 sm:w-auto disabled:text-white disabled:bg-gray-300 disabled:cursor-not-allowed'
+            onClick={action}
+            disabled={disabled}
+          >
+            Hapus
+          </button>
+        </div>
+      </div>
+    </Modal>
+  )
+}
+
+export default FieldOrderModal
