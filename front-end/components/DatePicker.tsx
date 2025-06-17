4a91cb12890078474e1f6f45da7311f271e2cc20
Abram Fernando Haullussy <abramfernando18@gmail.com>
2025-06-17 17:57:54 +0700

    refactor(front-end/components): change date picker component styling, so it allow user to click on entire input

--- front-end/components/DatePicker.tsx
+++ front-end/components/DatePicker.tsx
@@ -30,18 +30,25 @@ const DatePicker: FC<DatePickerProps> = ({ label, name, value, onChange, min, ma
         />
       )}
 
-      <input
-        ref={inputRef}
-        id={name}
-        type='date'
-        name={name}
-        value={value}
-        onChange={(e) => onChange(e.target.value)}
-        min={min}
-        max={max}
-        onFocus={handleClick}
-        className='px-3 py-2 border rounded-lg shadow-sm text-sm border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 text-gray-800 h-10'
-      />
+      <div className='relative w-full overflow-hidden rounded-md group'>
+        <input
+          ref={inputRef}
+          id={name}
+          type='date'
+          name={name}
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+          min={min}
+          max={max}
+          onFocus={handleClick}
+          className='px-3 py-2 border rounded-md shadow-sm text-sm border-gray-300 focus:outline-2 focus:outline-indigo-600 focus:border-indigo-600 text-gray-800 h-10 w-full group-hover:bg-gray-50'
+        />
+
+        <FormLabel
+          name={name}
+          className='absolute top-0 left-0 z-[1] w-full h-full opacity-0 group-hover:cursor-pointer'
+        />
+      </div>
     </div>
   )
 }
