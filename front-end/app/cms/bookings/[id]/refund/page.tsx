6e7a30e7ae8a224d53c93a69e5e3d2943154f159
Abram Fernando Haullussy <abramfernando18@gmail.com>
2025-06-20 14:42:36 +0700

    feat(front-end/app): add refund payment page component

---
+++ front-end/app/cms/bookings/[id]/refund/page.tsx
@@ -0,0 +1,187 @@
+'use client'
+import CMSLayout from '@/layouts/cms'
+import { BreadcrumbData } from '@/components/cms/Breadcrumbs'
+import Header from '@/components/cms/Header'
+import { useForm } from 'react-hook-form'
+import FormInput from '@/components/cms/FormInput'
+import FormLabel from '@/components/cms/FormLabel'
+import ImageUploader from '@/components/cms/ImageUploader'
+import { CreateEditFieldRequest, DetailAssetResponse, DetailFieldResponse, UploadAssetRequest } from '@/utils/type'
+import { cleanNumber, fetchWithAuth } from '@/utils/helper'
+import { useMutation } from '@tanstack/react-query'
+import { ENV } from '@/utils/constants'
+import toast from 'react-hot-toast'
+import { useParams, useRouter } from 'next/navigation'
+
+export default function CreateField() {
+  const router = useRouter()
+  const params = useParams()
+
+  const {
+    handleSubmit,
+    control,
+    register,
+    setValue,
+    clearErrors,
+    formState: { errors },
+  } = useForm<CreateEditFieldRequest>({
+    defaultValues: {
+      name: '',
+      pricePerHour: '',
+      photo: '',
+    },
+  })
+
+  const {
+    isPending: isPendingCreateField,
+    isSuccess: isSuccessCreateField,
+    mutate: mutateCreateField,
+  } = useMutation<DetailFieldResponse, unknown, CreateEditFieldRequest>({
+    mutationFn: async (data) => {
+      const response = await fetchWithAuth('cms', `${ENV.API_URL}/fields/add`, {
+        method: 'POST',
+        body: JSON.stringify(data),
+      })
+
+      const json = await response.json()
+
+      if (!response.ok) {
+        // Attach the JSON error message if needed
+        throw new Error(json.message || 'Create field failed')
+      }
+
+      return json
+    },
+    onSuccess: () => {
+      toast.success('Lapangan berhasil ditambahkan')
+
+      router.push('/cms/fields')
+    },
+    onError: () => {
+      toast.error('Gagal menambahkan lapangan')
+    },
+  })
+
+  const { isPending: isPendingUploadFieldPhoto, mutate: mutateUploadFieldPhoto } = useMutation<
+    DetailAssetResponse,
+    unknown,
+    UploadAssetRequest
+  >({
+    mutationFn: async (data) => {
+      const formData = new FormData()
+      formData.append('image', data.file)
+
+      const response = await fetchWithAuth('cms', `${ENV.API_URL}/assets/upload`, {
+        method: 'POST',
+        body: formData,
+      })
+
+      const json = await response.json()
+
+      if (!response.ok) {
+        // Attach the JSON error message if needed
+        throw new Error(json.message || 'Upload field photo failed')
+      }
+
+      return json
+    },
+    onSuccess: (data) => {
+      toast.success('Foto lapangan berhasil ditambahkan')
+
+      setValue('photo', data.data.imageUrl)
+    },
+    onError: () => {
+      toast.error('Gagal menambahkan foto lapangan')
+    },
+  })
+
+  const breadcrumbsPages: BreadcrumbData[] = [
+    { name: 'List Booking', path: '/cms/bookings', current: false },
+    { name: 'Detail Booking', path: `/cms/bookings/${params.id}`, current: false },
+    { name: 'Refund Booking', path: `/cms/bookings/${params.id}/refund`, current: true },
+  ]
+
+  const handleDropImage = async (droppedFile: File) => {
+    mutateUploadFieldPhoto({
+      contentType: droppedFile.type,
+      file: droppedFile,
+    })
+
+    clearErrors('photo')
+  }
+
+  const handleRemoveImage = () => {
+    setValue('photo', '')
+  }
+
+  const onSubmit = (data: CreateEditFieldRequest) => {
+    mutateCreateField({
+      ...data,
+      pricePerHour: cleanNumber(data.pricePerHour) as unknown as string,
+    })
+  }
+
+  return (
+    <CMSLayout pages={breadcrumbsPages}>
+      <Header
+        title='Refund Booking'
+        backUrl={breadcrumbsPages[1].path}
+      />
+
+      <form
+        className='flex flex-col items-center space-y-6 mt-4 w-full'
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <FormInput
+          control={control}
+          id='name'
+          name='name'
+          type='text'
+          label='Catatan'
+          placeholder='Ketikan catatan/alasan refund booking'
+          register={register}
+          rules={{
+            required: {
+              value: true,
+              message: 'Catatan/alasan refund booking wajib diisi',
+            },
+          }}
+          errors={errors}
+          wrapperClassName='w-full'
+        />
+
+        <div className='flex flex-col space-y-2 w-full'>
+          <FormLabel
+            label='Bukti Transfer'
+            name='photo'
+          />
+
+          <ImageUploader
+            control={control}
+            name='photo'
+            label='Foto Bukti Transfer Refund'
+            placeholder='Klik untuk mengupload atau drag foto ke inputan ini'
+            rules={{
+              required: {
+                value: true,
+                message: 'Foto bukti transfer refund wajib diisi!',
+              },
+            }}
+            errors={errors}
+            handleDrop={handleDropImage}
+            handleRemove={handleRemoveImage}
+            uploading={isPendingUploadFieldPhoto}
+          />
+        </div>
+
+        <button
+          type='submit'
+          className='flex items-center space-x-2 rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:bg-gray-300 disabled:cursor-not-allowed'
+          disabled={isPendingCreateField || isSuccessCreateField || isPendingUploadFieldPhoto}
+        >
+          Submit
+        </button>
+      </form>
+    </CMSLayout>
+  )
+}
