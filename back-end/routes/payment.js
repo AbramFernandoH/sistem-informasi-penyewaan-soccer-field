9b7c9a7a4416a511b4d23ec59254578b09392ff8
Abram Fernando Haullussy <abramfernando18@gmail.com>
2025-06-21 17:11:26 +0700

    feat(back-end/routes): add detail payment endpoint for cms

--- back-end/routes/payment.js
+++ back-end/routes/payment.js
@@ -90,4 +90,44 @@ router.get('/', requireAuth('cms'), async (req, res) => {
     }
 });
 
+router.get('/:paymentId/detail', requireAuth('cms'), async (req, res) => {
+    try {
+        const payment = await Payment.findById(req.params.paymentId).populate([
+            {
+                path: 'booking',
+                populate: {
+                    path: 'field',
+                    model: 'Field',
+                },
+            },
+            {
+                path: 'user',
+                model: 'User',
+            },
+        ]);
+
+        if (payment !== null) {
+            return res.status(200).json({
+                code: 200,
+                success: true,
+                message: 'OK',
+                data: payment,
+            });
+        } else {
+            return res.status(404).json({
+                code: 404,
+                success: false,
+                message: 'Not found',
+                data: null,
+            });
+        }
+    } catch {
+        return res.status(500).json({
+            code: 500,
+            success: false,
+            message: 'Failed to get detail payment',
+        });
+    }
+});
+
 module.exports = router;
