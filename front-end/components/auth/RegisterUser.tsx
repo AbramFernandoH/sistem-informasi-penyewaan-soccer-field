e8f2e5bf6500250493b49da2e9156f1a5cc5896b
Abram Fernando Haullussy <abramfernando18@gmail.com>
2025-06-21 22:35:14 +0700

    refactor(front-end/components): adjust login & register button styling

--- front-end/components/auth/RegisterUser.tsx
+++ front-end/components/auth/RegisterUser.tsx
@@ -315,7 +315,7 @@ export default function RegisterUser() {
                   <div className='flex flex-col space-y-3'>
                     <button
                       type='submit'
-                      className='flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+                      className='flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:bg-gray-200 disabled:hover:cursor-not-allowed'
                       disabled={isPending || isSuccess}
                     >
                       Submit
