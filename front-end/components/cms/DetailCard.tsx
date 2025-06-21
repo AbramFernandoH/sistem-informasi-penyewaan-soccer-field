33460a2b2a9c0d66403a0333b56c9449df1b1313
Abram Fernando Haullussy <abramfernando18@gmail.com>
2025-06-21 11:15:41 +0700

    refactor(front-end/components): adjust styling for detail card component

--- front-end/components/cms/DetailCard.tsx
+++ front-end/components/cms/DetailCard.tsx
@@ -13,7 +13,9 @@ const DetailCard: FC<DetailCardProps> = ({ wrapperClassName, children, title }) 
         <h2 className='font-bold text-lg'>{title}</h2>
       </div>
 
-      <div className='px-4 py-6 box-border flex flex-col space-y-8 divider-y divider-gray-300'>{children}</div>
+      <div className='px-4 py-6 box-border flex flex-col space-y-8 divider-y divider-gray-300 overflow-x-hidden'>
+        {children}
+      </div>
     </div>
   )
 }
