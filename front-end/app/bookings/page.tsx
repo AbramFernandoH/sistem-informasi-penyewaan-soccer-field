d726cf065fa8ec9d24fe53b800e01784303e010e
Abram Fernando Haullussy <abramfernando18@gmail.com>
2025-06-22 11:17:09 +0700

    feat(front-end/app): add list booking api implementation on customer side

--- front-end/app/bookings/page.tsx
+++ front-end/app/bookings/page.tsx
@@ -1,226 +1,83 @@
-import React from 'react'
+'use client'
+import React, { ReactNode, useEffect, useState } from 'react'
 import PwaLayout from '@/layouts/pwa'
 import Table from '@/components/Table'
 import { format, parseISO } from 'date-fns'
 import { id } from 'date-fns/locale'
-import { formatToRupiah, timeSlotsString } from '@/utils/helper'
+import { fetchWithAuth, formatToRupiah, timeSlotsString } from '@/utils/helper'
 import BookingBadge from '@/components/BookingBadge'
 import Link from 'next/link'
 import { EyeIcon } from '@heroicons/react/24/outline'
+import { useQuery } from '@tanstack/react-query'
+import { ListBookingRequest, ListBookingResponse } from '@/utils/type'
+import { ENV } from '@/utils/constants'
+import { userProfileStore } from '@/stores/userProfile'
 
 const Bookings = () => {
+  const { user } = userProfileStore((state) => state)
+
+  const [skip, setSkip] = useState(0)
+  const [page, setPage] = useState(1)
+  const [tableData, setTableData] = useState<(string | ReactNode)[][]>([])
+
   const tableHeaders = ['Lapangan', 'Tanggal', 'Waktu', 'Harga', 'Status', 'Action']
-  const tableData = [
-    [
-      'Lapangan 1',
-      format(parseISO('2025-06-21T10:13:50.126Z'), 'd MMMM yyyy', { locale: id }),
-      timeSlotsString([7, 8]),
-      formatToRupiah(250000),
-      <BookingBadge
-        key={`booking-status-1`}
-        status='fully_paid'
-      />,
-      <Link
-        key={1}
-        href={`/bookings/1`}
-        className='w-fit flex items-center space-x-2 rounded-md bg-indigo-600 px-3 py-2 text-center text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-      >
-        <EyeIcon className='size-3' />
 
-        <span>Detail</span>
-      </Link>,
-    ],
-    [
-      'Lapangan 1',
-      format(parseISO('2025-06-21T10:13:50.126Z'), 'd MMMM yyyy', { locale: id }),
-      timeSlotsString([7, 8]),
-      formatToRupiah(250000),
-      <BookingBadge
-        key={`booking-status-1`}
-        status='fully_paid'
-      />,
-      <Link
-        key={1}
-        href={`/bookings/1`}
-        className='w-fit flex items-center space-x-2 rounded-md bg-indigo-600 px-3 py-2 text-center text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-      >
-        <EyeIcon className='size-3' />
+  const { data: dataListBooking, isSuccess: isSuccessListBooking } = useQuery<
+    ListBookingRequest,
+    unknown,
+    ListBookingResponse
+  >({
+    queryKey: ['booking', skip],
+    enabled: user !== null,
+    refetchOnWindowFocus: false,
+    queryFn: async () => {
+      const res = await fetchWithAuth('pwa', `${ENV.API_URL}/bookings/${user?._id ?? ''}/customer?skip=${skip}`)
 
-        <span>Detail</span>
-      </Link>,
-    ],
-    [
-      'Lapangan 1',
-      format(parseISO('2025-06-21T10:13:50.126Z'), 'd MMMM yyyy', { locale: id }),
-      timeSlotsString([7, 8]),
-      formatToRupiah(250000),
-      <BookingBadge
-        key={`booking-status-1`}
-        status='fully_paid'
-      />,
-      <Link
-        key={1}
-        href={`/bookings/1`}
-        className='w-fit flex items-center space-x-2 rounded-md bg-indigo-600 px-3 py-2 text-center text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-      >
-        <EyeIcon className='size-3' />
+      if (!res.ok) {
+        const errorData = await res.json()
 
-        <span>Detail</span>
-      </Link>,
-    ],
-    [
-      'Lapangan 1',
-      format(parseISO('2025-06-21T10:13:50.126Z'), 'd MMMM yyyy', { locale: id }),
-      timeSlotsString([7, 8]),
-      formatToRupiah(250000),
-      <BookingBadge
-        key={`booking-status-1`}
-        status='fully_paid'
-      />,
-      <Link
-        key={1}
-        href={`/bookings/1`}
-        className='w-fit flex items-center space-x-2 rounded-md bg-indigo-600 px-3 py-2 text-center text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-      >
-        <EyeIcon className='size-3' />
+        throw new Error(errorData.message || 'Failed to get list booking')
+      }
 
-        <span>Detail</span>
-      </Link>,
-    ],
-    [
-      'Lapangan 1',
-      format(parseISO('2025-06-21T10:13:50.126Z'), 'd MMMM yyyy', { locale: id }),
-      timeSlotsString([7, 8]),
-      formatToRupiah(250000),
-      <BookingBadge
-        key={`booking-status-1`}
-        status='fully_paid'
-      />,
-      <Link
-        key={1}
-        href={`/bookings/1`}
-        className='w-fit flex items-center space-x-2 rounded-md bg-indigo-600 px-3 py-2 text-center text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-      >
-        <EyeIcon className='size-3' />
+      return res.json()
+    },
+  })
 
-        <span>Detail</span>
-      </Link>,
-    ],
-    [
-      'Lapangan 1',
-      format(parseISO('2025-06-21T10:13:50.126Z'), 'd MMMM yyyy', { locale: id }),
-      timeSlotsString([7, 8]),
-      formatToRupiah(250000),
-      <BookingBadge
-        key={`booking-status-1`}
-        status='fully_paid'
-      />,
-      <Link
-        key={1}
-        href={`/bookings/1`}
-        className='w-fit flex items-center space-x-2 rounded-md bg-indigo-600 px-3 py-2 text-center text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-      >
-        <EyeIcon className='size-3' />
+  const handleClickPrev = () => {
+    setSkip(skip > 0 ? skip - 10 : 0)
+    setPage(page > 1 ? page - 1 : 1)
+  }
 
-        <span>Detail</span>
-      </Link>,
-    ],
-    [
-      'Lapangan 1',
-      format(parseISO('2025-06-21T10:13:50.126Z'), 'd MMMM yyyy', { locale: id }),
-      timeSlotsString([7, 8]),
-      formatToRupiah(250000),
-      <BookingBadge
-        key={`booking-status-1`}
-        status='fully_paid'
-      />,
-      <Link
-        key={1}
-        href={`/bookings/1`}
-        className='w-fit flex items-center space-x-2 rounded-md bg-indigo-600 px-3 py-2 text-center text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-      >
-        <EyeIcon className='size-3' />
+  const handleClickNext = () => {
+    setSkip(skip + 10)
+    setPage(page + 1)
+  }
 
-        <span>Detail</span>
-      </Link>,
-    ],
-    [
-      'Lapangan 1',
-      format(parseISO('2025-06-21T10:13:50.126Z'), 'd MMMM yyyy', { locale: id }),
-      timeSlotsString([7, 8]),
-      formatToRupiah(250000),
-      <BookingBadge
-        key={`booking-status-1`}
-        status='fully_paid'
-      />,
-      <Link
-        key={1}
-        href={`/bookings/1`}
-        className='w-fit flex items-center space-x-2 rounded-md bg-indigo-600 px-3 py-2 text-center text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-      >
-        <EyeIcon className='size-3' />
+  useEffect(() => {
+    if (isSuccessListBooking && dataListBooking && dataListBooking.data) {
+      setTableData(
+        dataListBooking.data.items.map((booking) => [
+          booking.field.name,
+          format(parseISO(booking.orderDate), 'd MMMM yyyy', { locale: id }),
+          timeSlotsString(booking.timeSlots),
+          formatToRupiah(booking.price),
+          <BookingBadge
+            key={`booking-status-${booking._id}`}
+            status={booking.status}
+          />,
+          <Link
+            key={booking._id}
+            href={`/bookings/${booking._id}`}
+            className='w-fit flex items-center space-x-2 rounded-md bg-indigo-600 px-3 py-2 text-center text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+          >
+            <EyeIcon className='size-3' />
 
-        <span>Detail</span>
-      </Link>,
-    ],
-    [
-      'Lapangan 1',
-      format(parseISO('2025-06-21T10:13:50.126Z'), 'd MMMM yyyy', { locale: id }),
-      timeSlotsString([7, 8]),
-      formatToRupiah(250000),
-      <BookingBadge
-        key={`booking-status-1`}
-        status='fully_paid'
-      />,
-      <Link
-        key={1}
-        href={`/bookings/1`}
-        className='w-fit flex items-center space-x-2 rounded-md bg-indigo-600 px-3 py-2 text-center text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-      >
-        <EyeIcon className='size-3' />
-
-        <span>Detail</span>
-      </Link>,
-    ],
-    [
-      'Lapangan 1',
-      format(parseISO('2025-06-21T10:13:50.126Z'), 'd MMMM yyyy', { locale: id }),
-      timeSlotsString([7, 8]),
-      formatToRupiah(250000),
-      <BookingBadge
-        key={`booking-status-1`}
-        status='fully_paid'
-      />,
-      <Link
-        key={1}
-        href={`/bookings/1`}
-        className='w-fit flex items-center space-x-2 rounded-md bg-indigo-600 px-3 py-2 text-center text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-      >
-        <EyeIcon className='size-3' />
-
-        <span>Detail</span>
-      </Link>,
-    ],
-    [
-      'Lapangan 1',
-      format(parseISO('2025-06-21T10:13:50.126Z'), 'd MMMM yyyy', { locale: id }),
-      timeSlotsString([7, 8]),
-      formatToRupiah(250000),
-      <BookingBadge
-        key={`booking-status-1`}
-        status='fully_paid'
-      />,
-      <Link
-        key={1}
-        href={`/bookings/1`}
-        className='w-fit flex items-center space-x-2 rounded-md bg-indigo-600 px-3 py-2 text-center text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-      >
-        <EyeIcon className='size-3' />
-
-        <span>Detail</span>
-      </Link>,
-    ],
-  ]
+            <span>Detail</span>
+          </Link>,
+        ])
+      )
+    }
+  }, [isSuccessListBooking, dataListBooking])
 
   return (
     <PwaLayout>
@@ -230,6 +87,10 @@ const Bookings = () => {
         <Table
           headers={tableHeaders}
           data={tableData}
+          totalData={dataListBooking?.data.metadata.count ?? 0}
+          currentPage={page}
+          handleClickPrev={handleClickPrev}
+          handleClickNext={handleClickNext}
         />
       </div>
     </PwaLayout>
