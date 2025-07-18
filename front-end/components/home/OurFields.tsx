1fde9b3d27a5d0ab11fed1801c2d1fe5772b0448
Abram Fernando Haullussy <abramfernando18@gmail.com>
2025-07-18 08:42:30 +0700

    feat(front-end/component): add text for telling user to click one of the field card to open booking form

--- front-end/components/home/OurFields.tsx
+++ front-end/components/home/OurFields.tsx
@@ -58,6 +58,8 @@ const OurFields: FC<OurFieldsProps> = ({ titleClassName = '', customBackgroundCo
       <div className='mx-auto max-w-2xl px-6 py-24 lg:max-w-7xl lg:px-8'>
         <h2 className={`text-2xl font-bold text-gray-900 text-center ${titleClassName}`}>Lapangan Kami</h2>
 
+        <p className='mt-2 text-center'>Klik salah satu lapangan untuk membuka form pemesanan.</p>
+
         <div className='mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
           {!isPending && data
             ? data.data.items.map((field, idx) => (
