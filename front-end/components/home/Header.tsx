aa06c40d5c245384be596a0fdc95a0fe3340b801
Abram Fernando Haullussy <abramfernando18@gmail.com>
2025-07-18 08:50:01 +0700

    feat(front-end/component): add telephone link to contact now link

--- front-end/components/home/Header.tsx
+++ front-end/components/home/Header.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import React from 'react'
+import Link from 'next/link'
 
 const Header = () => {
   return (
@@ -6,13 +9,12 @@ const Header = () => {
       <div className='flex flex-col space-y-4 justify-center items-center p-6 rounded-lg bg-black/50 w-[calc(100%-30px)] md:w-fit'>
         <h1 className='text-[36px] text-green-500 font-bold'>Goedang Futsal</h1>
 
-        {/* TODO: add link for this button for redirect to cp */}
-        <button
-          type='button'
+        <Link
+          href='tel:0218509394'
           className='w-fit bg-white hover:bg-slate-100 border border-solid border-green-500 rounded-lg p-2 box-border text-green-500 font-medium text-lg hover:cursor-pointer'
         >
           Kontak Sekarang
-        </button>
+        </Link>
       </div>
     </header>
   )
